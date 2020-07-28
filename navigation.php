bf897b1a0742c2026526eef3bf8fc7a86c56b784
Wiktor Poprawski <kserix.xamer@live.com>
2020-07-28 15:36:39 +0200

    removal of outdated file structure

--- navigation.php
+++
@@ -1,8 +0,0 @@
-	<div class="navButtons">
-		<a href="logout.php" class="logout"><button class="button">Wyloguj się</button></a>
-		<a href="index.php" class="logout"><button class="button">Index.php</button></a>
-		<a href="manager.php" class="logout"><button class="button">Manager.php</button></a>
-		<a href="view_client.php" class="logout"><button class="button">View client.php</button></a>
-		<a href="employee.php" class="logout"><button class="button">Employee.php</button></a>
-		<a href="archive.php" class="logout"><button class="button">Archive.php</button></a>
-	</div>
--- script.js
+++
@@ -1,7 +0,0 @@
-
-setTimeout(function(){
-document.querySelector(".sqlResult").classList.add("transparent");
-	setTimeout(function(){
-		document.querySelector(".sqlResult").classList.add("hidden");
-	}, 1000);
-}, 3000);
