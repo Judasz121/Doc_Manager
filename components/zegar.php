b7e02a28349825df2887a060a8bfc6ed1ebdd338
Wiktor Poprawski <kserix.xamer@live.com>
2020-08-10 14:59:58 +0200

    Removal of old files

--- components/zegar.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$data1 = "2019-03-05 09:52:33";
-$datetime1 = new DateTime('NOW');
-$datetime2 = new DateTime($data1);
-$interval = $datetime1->diff($datetime2);
-echo $interval->format('%R%a days');
-
-?>
--- js/script_duplicate.js
+++
@@ -1,7 +0,0 @@
-var form = document.querySelector("form.doc-list");
-form.onsubmit = function(){
-    var btn = document.querySelector("input[type=submit]:focus");
-    var id = btn.getAttribute("id");
-    id = id.split("e")['1'];
-    btn.setAttribute("value", id);
-}
